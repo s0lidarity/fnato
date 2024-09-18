688fbcf2fcde1e32c6b20810848e5cddaa8378aa
Andrew Sobiesiak <andrew.sobiesiak@gmail.com>
2024-09-17 21:29:10 -0700

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+};
