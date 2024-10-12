770b8e1e2d6371e342e4b27a3e454c5d59dde1bc
Andrew Sobiesiak <andrew.sobiesiak@gmail.com>
2024-10-11 20:52:44 -0700

    feature: tooltip for stats labels

--- src/pages/TheCrucible/Statistics/StatInput.tsx
+++ src/pages/TheCrucible/Statistics/StatInput.tsx
@@ -1,10 +1,34 @@
 import { h } from 'preact';
-import { NumberInput } from 'react95';
+import { NumberInput, Tooltip } from 'react95';
+import styled from 'styled-components';
+import { STAT_REMINDERS } from '../../../types/characterTypes';
+
+
+// AJS apply the styling to the span inside of the text within the tooltip instead of the tooltip itself
+const StyledTooltip = styled(Tooltip)`
+	& .tooltip-content {
+		background-color: ${({ theme }) => theme.canvas};
+		color: ${({ theme }) => theme.canvasText};
+		border: 1px solid ${({ theme }) => theme.borderDark};
+		padding: 0.5rem;
+		font-size: 1rem;
+	}
+`;
+
+// style the NumberInput, it's too long
 
 function StatInput({ label, value, onChange }) {
 	return (
 		<div>
-			<label>{label}</label>
+			<StyledTooltip className='tooltip-content' text={
+					<span style={{color: 'black'}}>
+						{STAT_REMINDERS[label.toLowerCase()]}
+					</span>
+				} enterDelay={100} leaveDelay={500}>
+				<label>
+					{label}
+				</label>
+			</StyledTooltip>
 			<NumberInput
 				min={3}
 				max={18}
