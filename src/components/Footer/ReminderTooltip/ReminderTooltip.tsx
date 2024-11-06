cf0cbc68a704528ff47848f23be5d4cff7fd927e
Andrew Sobiesiak <andrew.sobiesiak@gmail.com>
2024-11-05 16:21:41 -0800

    feat: profession
    made the ? tooltip indicator its own component

--- src/components/Footer/ReminderTooltip/ReminderTooltip.tsx
+++ src/components/Footer/ReminderTooltip/ReminderTooltip.tsx
@@ -1,6 +1,8 @@
 import { Tooltip } from 'react95';
 import styled from 'styled-components';
 
+import TooltipIndicator from '../../TooltipIndicator/TooltipIndicator';
+
 // AJS this is a shared style, put it somewhere we can share it
 const StyledTooltipInnerText = styled.span`
     padding: 0.5rem;
@@ -13,24 +15,10 @@ const StyledLabel = styled.label`
     align-items: center;
 `;
 
-const StyledQuestionmark = styled.span`
-    display: flex;
-    margin-left: 0.25rem;
-    font-size: 0.75rem;
-    justify-content: center;
-    width: 1rem;
-    height: 1rem;
-    border-radius: 50%;
-    background-color: ${({ theme }) => theme.materialDark};
-    color: ${({ theme }) => theme.materialLight};
-    padding: 0.1rem;
-    cursor: help;
-`;
-
 interface ReminderTooltipProps {
     itemKey: string;
     labelText: string;
-    // AJS rework reminder to look at the value from the skill instead of the hard-look up
+    // AJS refactor reminder to look at the value from the skill instead of the hard-look up
     reminders: { [key: string]: string };
 }
 
@@ -47,7 +35,7 @@ export function ReminderTooltip({ itemKey, labelText, reminders }: ReminderToolt
             enterDelay={100}
             leaveDelay={500}
         >
-            <StyledLabel>{labelText}<StyledQuestionmark>?</StyledQuestionmark></StyledLabel>
+            <StyledLabel>{labelText}<TooltipIndicator /></StyledLabel>
         </Tooltip>
     );
 };
---
+++ src/components/TooltipIndicator/TooltipIndicator.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const StyledQuestionmark = styled.span`
+    display: flex;
+    margin-left: 0.25rem;
+    font-size: 0.75rem;
+    justify-content: center;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.materialDark};
+    color: ${({ theme }) => theme.materialLight};
+    padding: 0.1rem;
+    cursor: help;
+`;
+
+function TooltipIndicator() {
+    return <StyledQuestionmark>?</StyledQuestionmark>;
+}
+
+export default TooltipIndicator;
