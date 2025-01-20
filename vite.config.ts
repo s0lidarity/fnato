d165217eed9fa2ba8e98ace90f08dc6b1c0d139e
Andrew Sobiesiak <andrew.sobiesiak@gmail.com>
2025-01-19 22:20:33 -0800

    feat: switching to lingui

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import { readFileSync } from 'fs';
-
+import { lingui } from '@lingui/vite-plugin';
 const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [preact({
+		babel: {
+			plugins: ['@lingui/babel-plugin-lingui-macro'],
+		},
+		}),
+		lingui(),
+	],
 	define: {
 		'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version),
 	},
