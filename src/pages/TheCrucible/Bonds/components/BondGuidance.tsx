62d7fb4a29527be9a383f853e3641e4bbd9f445a
Andrew Sobiesiak <andrew.sobiesiak@gmail.com>
2025-02-15 22:22:57 -0800

    feat: trans
    fixed navigation menu and started on bond guidance

--- src/pages/TheCrucible/Bonds/components/BondGuidance.tsx
+++ src/pages/TheCrucible/Bonds/components/BondGuidance.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { Table, TableBody, TableHead, TableRow, TableHeadCell, TableDataCell } from 'react95';
+import { t } from '@lingui/core/macro';
+import { Trans } from '@lingui/react/macro';
 
 import Guidance from '../../../../components/Guidance/Guidance';
 import { useStats } from '../../../../providers/StatisticsContext';
@@ -22,6 +24,7 @@ const BondPrimer = styled.p.attrs<any>({
 function BondGuidance() {
     const { stats } = useStats();
 
+    // AJS start here, make these msgs instead of strings
     const bondExamples = [
         { bondType: 'Individual', example: 'Spouse or ex-spouse' },
         { bondType: 'Individual', example: 'Coworker or partner' },
@@ -40,21 +43,22 @@ function BondGuidance() {
 
     return (
         <BondGuidanceContainer>
-            <Guidance title="Bonds Details" buttonText="Bonds Details">
+            <Guidance title={t`Bonds Details`} buttonText={t`Bonds Details`}>
                 <BondPrimer>
-                    {`Bonds represent your Agent's relationships with other people.
+                    <Trans>
+                        {`Bonds represent your Agent's relationships with other people.
                     Each bond starts with a score equal to your Agent's Charisma (${stats.charisma.score}).
                     The score of a bond can improve if it is nurtured between operations. A bond can never have a score higher than your Agent's Charisma.
                     Any time your Agent's Charisma drops, each bond score drops by the same amount.
                     Bonds can be defined as a single person or a group of people. For example, an agent might have bonds with their spouse, their business partner, and their bowling league.
-                    A Bond must be a real person or small group of people who are alive and can be interacted with.
-                    `}
+                    A Bond must be a real person or small group of people who are alive and can be interacted with.`}
+                    </Trans>
                 </BondPrimer>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableHeadCell>Bond Type</TableHeadCell>
-                            <TableHeadCell>Example</TableHeadCell>
+                            <TableHeadCell>{t`Bond Type`}</TableHeadCell>
+                            <TableHeadCell>{t`Example`}</TableHeadCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
