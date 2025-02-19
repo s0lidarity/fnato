2a3d594a9aa8a63ae5e071053adb62be0d7ce14b
Andrew Sobiesiak <andrew.sobiesiak@gmail.com>
2025-02-18 22:43:08 -0800

    feat: drivethru affiliate

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 import { version } from '../../../package.json';
+import DriveThruBanner from '../DriveThruBanner/DriveThruBanner';
 
-const FooterWrapper = styled.div`
+const FooterWrapper = styled.div.attrs<any>({
+    'data-testid': 'footer-wrapper',
+    'data-component': 'Footer/Wrapper'
+})`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     width: 100%;
@@ -10,13 +15,18 @@ const FooterWrapper = styled.div`
     color: ${({ theme }) => theme.headerText};
 `;
 
-const VersionText = styled.span`
+const VersionText = styled.span.attrs<any>({
+    'data-testid': 'footer-version',
+    'data-component': 'Footer/VersionText'
+})`
     font-size: 0.8rem;
+    margin-bottom: 1rem;
 `;
 
 function Footer() {
     return (
         <FooterWrapper>
+            <DriveThruBanner />
             <VersionText>v{version}</VersionText>
         </FooterWrapper>
     );
